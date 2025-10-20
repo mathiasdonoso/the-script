a89e83fe5b6eb640f840ef0b2d824caebb0051ea
Mathias <mathiasd88@gmail.com>
2025-10-19 21:51:18 -0300

    feat: moving flake.nix location and updating flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,46 @@
 {
-  description = "A very basic flake";
+  description = "Dev environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.git
-        pkgs.neovim
-        pkgs.i3
-        pkgs.tmux
-        pkgs.alacritty
-        pkgs.picom
-        pkgs.jq
-        pkgs.feh
-        pkgs.stow
-        pkgs.fzf
-        pkgs.curl
-        pkgs.tldr
-        pkgs.fd
-        pkgs.ripgrep
-        pkgs.flameshot
-        pkgs.cmake
-        pkgs.gcc
-        pkgs.docker
-      ];
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      # Este es el buildEnv combinando todos los programas
+      packages.x86_64-linux.all = pkgs.buildEnv {
+        name = "all-programs";
+        paths = [
+          pkgs.git
+          pkgs.neovim
+          pkgs.i3
+          pkgs.tmux
+          pkgs.alacritty
+          pkgs.picom
+          pkgs.jq
+          pkgs.feh
+          pkgs.stow
+          pkgs.fzf
+          pkgs.curl
+          pkgs.tldr
+          pkgs.fd
+          pkgs.ripgrep
+          pkgs.flameshot
+          pkgs.cmake
+          pkgs.gcc
+          pkgs.docker
+        ];
+      };
+
+      # Opcional: devShell separado
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.neovim
+          pkgs.git
+        ];
+      };
     };
-  };
 }
