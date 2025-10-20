7a9ada05c82aeb785de745b54518f4411a618998
Mathias <mathiasd88@gmail.com>
2025-10-19 21:23:09 -0300

    feat: updating flake file

--- flake.nix
+++ flake.nix
@@ -1,36 +1,36 @@
 {
-  description = "Linux workstation";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.default = nixpkgs.lib.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        git
-        tmux
-        i3
-        jq
-        feh
-        stow
-        fzf
-        curl
-        tldr
-        fd
-        ripgrep
-        flameshot
-        cmake
-        gcc
-        docker
-        alacritty
-        picom
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.git
+        pkgs.neovim
+        pkgs.i3
+        pkgs.tmux
+        pkgs.alacritty
+        pkgs.picom
+        pkgs.jq
+        pkgs.feh
+        pkgs.stow
+        pkgs.fzf
+        pkgs.curl
+        pkgs.tldr
+        pkgs.fd
+        pkgs.ripgrep
+        pkgs.flameshot
+        pkgs.cmake
+        pkgs.gcc
+        pkgs.docker
       ];
-
-      shellHook = ''
-        echo "Loading environment..."
-        export EDITOR=nvim
-      '';
     };
   };
 }
